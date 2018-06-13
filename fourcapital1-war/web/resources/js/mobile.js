e0b18650a3b48a25134dc2746930e259c49e54f7
Usuario <Usuario@DESKTOP-7G0ITVH>
2018-06-13 11:20:31 -0500

    comit completo

---
+++ fourcapital1-war/web/resources/js/mobile.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+javascript:window.history.forward(1);
+
+var m = navigator.userAgent;
+if (m.match(/Mobile/i)) {
+    $('#htmlconsulta').addClass('ui-mobile');
+    $('#bodyconsulta').addClass('ui-mobile');
+}
+
+
+
+
